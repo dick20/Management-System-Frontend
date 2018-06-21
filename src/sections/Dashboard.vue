5007625b07d6222800a8a8c11755862e0fb848a1
Ecr23 <370095872@qq.com>
2018-06-21 17:15:41 +0800

    init dashboard

--- src/sections/Dashboard.vue
+++ src/sections/Dashboard.vue
@@ -1,13 +1,25 @@
 <template lang="jade">
   .monitor-page-dashboard
     .page-header
-      h1
-        | Dashboard
+      h1 Dashboard
+
 </template>
 
 <script>
+import api from '../service/api.js'
+
 export default {
-  name: 'Dashboard'
+  name: 'Dashboard',
+
+  ready: function () {
+    let param = {
+      'pageSize': 10,
+      'pageNumber': 1
+    }
+    api.getOrder(this, param.pageSize, param.pageNumber).then((res) => {
+      console.log(res.data)
+    })
+  }
 }
 </script>
 
--- src/service/api.js
+++ src/service/api.js
@@ -3,5 +3,9 @@ const apiRoot = 'http://111.230.31.38:8080'
 export default {
   getMenu: function (context) {
     return context.$http.get(apiRoot)
+  },
+
+  getOrder: function (context, pageSize, pageNum) {
+    return context.$http.get(apiRoot + '/restaurant/order?pageSize=' + pageSize + '?pageNum=' + pageNum)
   }
 }
