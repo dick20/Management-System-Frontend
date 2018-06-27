4bab29a2e14707b0ba7b48fd3f3164ca2ca9380a
BeAShaper <13136607660@163.com>
2018-06-27 15:07:43 +0800

    .

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import Resource from 'vue-resource'
 import VueHighcharts from 'vue-highcharts'
 import App from './App'
 import router from './router'
-import './sections/assets/iconfont/iconfont.css'
+import './assets/iconfont/iconfont.css'
 // Start up app
 Vue.use(Resource)
 Vue.use(VueHighcharts)
