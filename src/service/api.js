7dd1fced5636fd784c08790c8a57d47bcf1fe5ca
JoryLiu <liuzhaorui55@hotmail.com>
2018-06-28 20:05:02 +0800

    初版商家管理前端

--- src/service/api.js
+++
@@ -1,7 +0,0 @@
-let apiRoot = 'http://111.230.31.38:8080/'
-
-export default {
-  getMenu: function (context) {
-    return context.$http.get(apiRoot)
-  }
-}
--- src/service/machine.js
+++
@@ -1,8 +0,0 @@
-export default {
-  getMachineList: function (context) {
-    return context.$http.get('/api/machines/')
-  },
-  getOneMachineDetail: function (context, macId) {
-    return context.$http.get('/api/machines/' + macId)
-  }
-}
